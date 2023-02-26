68ea52ca557c1f84c0096c2d7c59fa07f64738e3
Kenlly-Chacon <kevinleninchacon@hotmail.com>
2023-02-26 00:46:06 -0500

    taller_37

---
+++ src/pages/Negado.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>403</h1>
+<h1>No esta autorizado ha acceder al recurso</h1>
+</template>
+
+<script>
+export default {
+  name: "Negado"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,17 +3,53 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 
 //Carga perezosa donde se trae bajo demanda
 const routes = [
-    {path: '/', component: () => import(/* webpackChunkName: "Inicio" */ '@/pages/Inicio.vue')},
-    {path: '/buscar/:idCliente', component: () => import(/* webpackChunkName: "Buscar" */ '@/pages/Buscar.vue')},
-    {path: '/actualizar', component: () => import(/* webpackChunkName: "Actualizar" */ '@/pages/Actualizar.vue')},
-    {path: '/eliminar', component: () => import(/* webpackChunkName: "Eliminar" */ '@/pages/Eliminar.vue')},
-    {path: '/insertar', component: () => import(/* webpackChunkName: "Insertar" */ '@/pages/Insertar.vue')},
-    {path: '/:pathMatch(.*)*', component: () => import(/* webpackChunkName: "NoFound" */ '@/pages/NoFound.vue')},
+    {path: '/', name: 'inicio', component: () => import(/* webpackChunkName: "Inicio" */ '@/pages/Inicio.vue')},
+    {
+        path: '/buscar/:idCliente',
+        name: 'buscar',
+        component: () => import(/* webpackChunkName: "Buscar" */ '@/pages/Buscar.vue')
+    },
+    {
+        path: '/actualizar',
+        name: 'actualizar',
+        component: () => import(/* webpackChunkName: "Actualizar" */ '@/pages/Actualizar.vue')
+    },
+    {
+        path: '/eliminar',
+        name: 'eliminar',
+        component: () => import(/* webpackChunkName: "Eliminar" */ '@/pages/Eliminar.vue')
+    },
+    {
+        path: '/insertar',
+        name: 'insertar',
+        component: () => import(/* webpackChunkName: "Insertar" */ '@/pages/Insertar.vue')
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: '404',
+        component: () => import(/* webpackChunkName: "NoFound" */ '@/pages/NoFound.vue')
+    },
+    {path: '/negado', name: '403', component: () => import(/* webpackChunkName: "Negado" */ '@/pages/Negado.vue')},
 ]
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
+})
+
+//GUARDIAN
+router.beforeEach((to, from, next) => {
+    console.log({to, from, next})
+    const random = Math.random() * 10
+    if (random > 5) {
+        console.log('Autorizado')
+        next()
+    } else {
+        console.log('No Autorizado')
+        next({name: '403'})
+    }
+    //para darle el paso al recuro que quiero
+    // next()
 })
 
 export default router
